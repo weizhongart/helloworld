a94534640b05c4427c865aca216fbc9d1a5fb7cc
weizhong <42662470+weizhongart@users.noreply.github.com>
2025-06-23 17:41:40 +0800

    推送本地更改以创建合并请求

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -13,5 +13,39 @@ public class HelloWorld {
 
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
+}
 
+// 新增 QuickSortUtils 排序工具类
+class QuickSortUtils {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                // 交换元素
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // 将 pivot 元素放到正确的位置
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
 }
